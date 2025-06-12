d476d9dac81cd21f0ff17713a40d8b86d2fe4d88
Maks747 <valol93@mail.ru>
2025-06-12 20:44:47 +0300

    Добавил тесты в ветке mid-level-step-objects

---
+++ demoqa_tests/model/pages/registration_page.py
@@ -0,0 +1,66 @@
+from selene import browser, command, have
+import os
+
+
+
+class RegistrationPage:
+    def __init__(self):
+        self.registered_user_with = browser.element('.table').all('td').even
+
+    def open(self):
+            browser.open('/automation-practice-form')
+
+    def fill_first_name(self, value):
+        browser.element('#firstName').type(value)
+
+    def fill_last_name(self, value):
+        browser.element('#lastName').type(value)
+
+    def fill_email(self, value):
+        browser.element('#userEmail').type(value)
+
+    def choose_gender(self, value):
+        browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
+
+    def fill_phone_number(self, value):
+        browser.element('#userNumber').type(value)
+
+    def fill_birthday(self, year, month, day):
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').type(month)
+        browser.element('.react-datepicker__year-select').type(year)
+        browser.element(
+            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+        ).click()
+
+    def fill_subjects(self, value):
+            browser.element('#subjectsInput').type(value).press_enter()
+
+    def fill_hobbies(self, value):
+        browser.all('#hobbiesWrapper label').element_by(have.exact_text(value)).click()
+
+    def fill_picture(self, file):
+        browser.element('#uploadPicture').set_value(
+            os.path.abspath(f'resources/{file}')
+            )
+
+    def fill_address(self, value):
+        browser.element('#currentAddress').type(value)
+
+    def fill_state(self, value):
+        browser.element('#state').click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(value)
+        ).click()
+
+    def fill_city(self, value):
+        browser.element('#city').click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(value)
+        ).click()
+
+    def submit(self):
+        browser.element('#submit').perform(command.js.scroll_into_view).click()
+
+    def should_open_form_with_text(self, value):
+        browser.element('#example-modal-sizes-title-lg').should(have.text(value))
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,29 +1,39 @@
-from selene import browser, command, have
-import os
+from demoqa_tests.model.pages.registration_page import RegistrationPage
+from selene import have
 
 def test_practice_form_filling():
-    browser.open('/automation-practice-form')
+    registration_page = RegistrationPage()
+    registration_page.open()
     # Заполняем поля
-    browser.element('#firstName').type('Андрей')
-    browser.element('#lastName').type('Смирнов')
-    browser.element('#userEmail').type('name123@example.com')
-    browser.element(f"//label[contains(text(),'Male')]").click()
-    browser.element('#userNumber').type('1234567891')
-    browser.element('#dateOfBirthInput').click()
-    browser.element(".react-datepicker__year-select").type('1990').click()
-    browser.element(".react-datepicker__month-select").element('[value="3"]').click()
-    browser.element(".react-datepicker__day--020").click()
-    browser.element('#subjectsInput').type('Computer Science').press_enter()
-    browser.element(f"//label[contains(text(),'Sports')]").click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('beautiful_tropical_beach_sea_ocean.png'))
-    browser.element('#currentAddress').perform(command.js.scroll_into_view).type('Street, 15 house')
-    browser.element('#state').click().element('#react-select-3-option-2').click()
-    browser.element('#city').click().element('#react-select-4-option-0').click()
+    registration_page.fill_first_name('Андрей')
+    registration_page.fill_last_name('Смирнов')
+    registration_page.fill_email('name123@example.com')
+    registration_page.choose_gender('Male')
+    registration_page.fill_phone_number('1234567891')
+    registration_page.fill_birthday('1990','April','20')
+    registration_page.fill_subjects('Computer Science')
+    registration_page.fill_hobbies('Sports')
+    registration_page.fill_picture('beautiful_tropical_beach_sea_ocean.png')
+    registration_page.fill_address('Street, 15 house')
+    registration_page.fill_state('Haryana')
+    registration_page.fill_city('Karnal')
 
     #Подтверждаем заполнение данных
-    browser.element('#submit').perform(command.js.scroll_into_view).click()
+    registration_page.submit()
 
     #Проверка после нажатия на кнопку submit
-    browser.element('.table-responsive').all('td').even.should(have.exact_texts
-    (f'Андрей Смирнов', 'name123@example.com', 'Male', '1234567891', '20 April,1990', 'Computer Science', 'Sports',
-     'beautiful_tropical_beach_sea_ocean.png', 'Street, 15 house', 'Haryana Karnal'))
+    registration_page.should_open_form_with_text('Thanks for submitting the form')
+    registration_page.registered_user_with.should(
+        have.texts(
+        'Андрей Смирнов',
+        'name123@example.com',
+        'Male',
+        '1234567891',
+        '20 April,1990',
+        'Computer Science',
+        'Sports',
+        'beautiful_tropical_beach_sea_ocean.png',
+        'Street, 15 house',
+        'Haryana Karnal'
+    )
+    )

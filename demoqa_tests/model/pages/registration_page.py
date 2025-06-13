09b3a9f779ceca31ba99259cee3cecedf45a88a5
Maks747 <valol93@mail.ru>
2025-06-13 14:47:34 +0300

    Добавил тесты в ветке high-level-step-objects

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -1,66 +1,50 @@
 from selene import browser, command, have
 import os
 
-
-
 class RegistrationPage:
-    def __init__(self):
-        self.registered_user_with = browser.element('.table').all('td').even
 
     def open(self):
             browser.open('/automation-practice-form')
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').type(value)
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').type(value)
-
-    def fill_email(self, value):
-        browser.element('#userEmail').type(value)
-
-    def choose_gender(self, value):
-        browser.all('[name=gender]').element_by(have.value(value)).element('..').click()
-
-    def fill_phone_number(self, value):
-        browser.element('#userNumber').type(value)
-
-    def fill_birthday(self, year, month, day):
+    def registers_user(self, user):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('#userEmail').type(user.email)
+        browser.all('[name=gender]').element_by(have.value(user.gender)).element('..').click()
+        browser.element('#userNumber').type(user.phone_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').type(month)
-        browser.element('.react-datepicker__year-select').type(year)
+        browser.element('.react-datepicker__month-select').type(user.month)
+        browser.element('.react-datepicker__year-select').type(user.year)
         browser.element(
-            f'.react-datepicker__day--0{day}:not(.react-datepicker__day--outside-month)'
+            f'.react-datepicker__day--0{user.day}:not(.react-datepicker__day--outside-month)'
         ).click()
-
-    def fill_subjects(self, value):
-            browser.element('#subjectsInput').type(value).press_enter()
-
-    def fill_hobbies(self, value):
-        browser.all('#hobbiesWrapper label').element_by(have.exact_text(value)).click()
-
-    def fill_picture(self, file):
+        browser.element('#subjectsInput').type(user.subjects).press_enter()
+        browser.all('#hobbiesWrapper label').element_by(have.exact_text(user.hobbies)).click()
         browser.element('#uploadPicture').set_value(
-            os.path.abspath(f'resources/{file}')
+            os.path.abspath(f'resources/{user.picture}')
             )
-
-    def fill_address(self, value):
-        browser.element('#currentAddress').type(value)
-
-    def fill_state(self, value):
+        browser.element('#currentAddress').type(user.address)
         browser.element('#state').click()
         browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(value)
+            have.exact_text(user.state)
         ).click()
-
-    def fill_city(self, value):
         browser.element('#city').click()
         browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(value)
+            have.exact_text(user.city)
         ).click()
-
-    def submit(self):
         browser.element('#submit').perform(command.js.scroll_into_view).click()
 
-    def should_open_form_with_text(self, value):
-        browser.element('#example-modal-sizes-title-lg').should(have.text(value))
+    def should_registered_user_with(self, user):
+        browser.element('.table').all('td').even.should(have.exact_texts(
+            f'{user.first_name} {user.last_name}',
+            user.email,
+            user.gender,
+            user.phone_number,
+            f'{user.day} {user.month},{user.year}',
+            user.subjects,
+            user.hobbies,
+            user.picture,
+            user.address,
+            f'{user.state} {user.city}'
+        )
+        )
--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,39 +1,26 @@
 from demoqa_tests.model.pages.registration_page import RegistrationPage
-from selene import have
+from demoqa_tests.data.users import User
 
 def test_practice_form_filling():
     registration_page = RegistrationPage()
     registration_page.open()
-    # Заполняем поля
-    registration_page.fill_first_name('Андрей')
-    registration_page.fill_last_name('Смирнов')
-    registration_page.fill_email('name123@example.com')
-    registration_page.choose_gender('Male')
-    registration_page.fill_phone_number('1234567891')
-    registration_page.fill_birthday('1990','April','20')
-    registration_page.fill_subjects('Computer Science')
-    registration_page.fill_hobbies('Sports')
-    registration_page.fill_picture('beautiful_tropical_beach_sea_ocean.png')
-    registration_page.fill_address('Street, 15 house')
-    registration_page.fill_state('Haryana')
-    registration_page.fill_city('Karnal')
 
-    #Подтверждаем заполнение данных
-    registration_page.submit()
+    test_user = User(
+    first_name='Андрей',
+    last_name = 'Смирнов',
+    email = 'name123@example.com',
+    gender = 'Male',
+    phone_number = '1234567891',
+    year = '1990',
+    month = 'April',
+    day = '20',
+    subjects = 'Computer Science',
+    hobbies = 'Sports',
+    picture = 'beautiful_tropical_beach_sea_ocean.png',
+    address = 'Street, 15 house',
+    state = 'Haryana',
+    city = 'Karnal'
+    )
 
-    #Проверка после нажатия на кнопку submit
-    registration_page.should_open_form_with_text('Thanks for submitting the form')
-    registration_page.registered_user_with.should(
-        have.texts(
-        'Андрей Смирнов',
-        'name123@example.com',
-        'Male',
-        '1234567891',
-        '20 April,1990',
-        'Computer Science',
-        'Sports',
-        'beautiful_tropical_beach_sea_ocean.png',
-        'Street, 15 house',
-        'Haryana Karnal'
-    )
-    )
+    registration_page.registers_user(test_user)
+    registration_page.should_registered_user_with(test_user)
